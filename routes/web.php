17672df718de9048bcb900a1382b512ef829a658
Matheus <nmmatheus@hotmail.com>
2025-04-22 16:30:58 -0300

    feat: menu de navegação e redirecionamento

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\DesenvolvedorController;
+use App\Http\Controllers\IndexController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/', [IndexController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +20,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('/admin')->middleware(App\Http\Middleware\AdminMiddleware::class)->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+    Route::resource('developers', DesenvolvedorController::class);
+});
+
+
+Route::get('/teste', function () {
+   dd(Auth::user(), Auth::user()->is_admin);
 });
 
 require __DIR__.'/auth.php';
